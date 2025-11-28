a85e542fb013e7e0ad56f39225622a70f510019a
Jack Ball <ball.jack2376@gmail.com>
2025-11-28 14:08:39 -0700

    Add -basic functionality in place

--- components/islands/QuoteGenerator.tsx
+++ components/islands/QuoteGenerator.tsx
@@ -2,164 +2,291 @@ import { useState } from 'react';
 import '../../styles/quoteGenerator.css';
 
 interface Service {
-    name: string;
-    price: number;
-    estimate: number;
+  name: string;
+  price: number;
+  estimate: number;
 }
 
 interface ServiceItem extends Service {
-    quantity: number;
+  quantity: number;
+}
+
+interface OtherService {
+  name: string;
+  price: number;
+}
+
+interface OtherServiceItem extends OtherService {
+  quantity: number;
 }
 
 interface ultraPremiums {
-    name: string;
-    dr: number;
-    da: number;
-    traffic: string;
-    price: number;
+  name: string;
+  dr: number;
+  da: number;
+  traffic: string;
+  price: number;
+  estimate: number;
 }
 
 interface ultraPremiumItems extends ultraPremiums {
-    quantity: number;
+  quantity: number;
 }
 
 interface IslandProps {
-    initialServices: Service[];
-    initialUltraPremiums: ultraPremiums[];
+  initialServices: Service[];
+  initialUltraPremiums: ultraPremiums[];
+  initialContentServices: OtherService[];
+  initialTechnicalServices: OtherService[];
 }
 
 export default function QuoteGenerator({
-    initialServices,
-    initialUltraPremiums,
+  initialServices,
+  initialUltraPremiums,
+  initialContentServices,
+  initialTechnicalServices,
 }: IslandProps): JSX.Element {
-    const [rows, setRows] = useState<ServiceItem[]>(() => {
-        if (!initialServices) return [];
-        return initialServices.map((service) => ({
-            ...service,
-            quantity: 0,
-        }));
+  const [rows, setRows] = useState<ServiceItem[]>(() => {
+    if (!initialServices) return [];
+    return initialServices.map((service) => ({
+      ...service,
+      quantity: 0,
+    }));
+  });
+  const [ultraPremiumRows, setUltraPremiumRows] = useState<ultraPremiumItems[]>(
+    () => {
+      if (!initialUltraPremiums) return [];
+      return initialUltraPremiums.map((premium) => ({
+        ...premium,
+        quantity: 0,
+      }));
+    },
+  );
+  const [contentServiceRows, setContentServiceRows] = useState<
+    OtherServiceItem[]
+  >(() => {
+    if (!initialContentServices) return [];
+    return initialContentServices.map((service) => ({
+      ...service,
+      quantity: 0,
+    }));
+  });
+  const [technicalServiceRows, setTechnicalServiceRows] = useState<
+    OtherServiceItem[]
+  >(() => {
+    if (!initialTechnicalServices) return [];
+    return initialTechnicalServices.map((service) => ({
+      ...service,
+      quantity: 0,
+    }));
+  });
+  const [showUltraPremium, setShowUltraPremium] = useState(false);
+  const [monthlyTerm, setMonthlyTerm] = useState(3);
+
+  const formatDollar = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  const updateQuantity = (type: string, index: number, value: number) => {
+    if (type === 'links') {
+      setRows((prevRows) => {
+        const newRows = [...prevRows];
+        newRows[index].quantity = value;
+        return newRows;
+      });
+    } else if (type === 'content') {
+      setContentServiceRows((prevRows) => {
+        const newRows = [...prevRows];
+        newRows[index].quantity = value;
+        return newRows;
+      });
+    } else if (type === 'technical') {
+      setTechnicalServiceRows((prevRows) => {
+        const newRows = [...prevRows];
+        newRows[index].quantity = value;
+        return newRows;
+      });
+    }
+  };
+
+  const premiumTotal: number = ultraPremiumRows.reduce(
+    (acc, row) => acc + row.price * row.quantity,
+    0,
+  );
+
+  const updatePremium = (index: number, value: number) => {
+    setUltraPremiumRows((prevRows) => {
+      const newRows = [...prevRows];
+      newRows[index].quantity = value;
+      return newRows;
     });
-    const [ultraPremiumRows, setUltraPremiumRows] = useState<ultraPremiumItems[]>(
-        () => {
-            if (!initialUltraPremiums) return [];
-            return initialUltraPremiums.map((premium) => ({
-                ...premium,
-                quantity: 0,
-            }));
-        },
-    );
-    const [showUltraPremium, setShowUltraPremium] = useState(false);
+  };
 
-    const updateQuantity = (index: number, value: number) => {
-        setRows((prevRows) => {
-            const newRows = [...prevRows];
-            newRows[index].quantity = value;
-            return newRows;
-        });
-    };
+  const linkPriceTotal: number = rows.reduce(
+    (acc, row) => acc + row.price * row.quantity,
+    0,
+  );
+  const contentPriceTotal = contentServiceRows.reduce(
+    (acc, row) => acc + row.price * row.quantity,
+    0,
+  );
+  const technicalPriceTotal = technicalServiceRows.reduce(
+    (acc, row) => acc + row.price * row.quantity,
+    0,
+  );
 
-    const premiumTotal = ultraPremiumRows.reduce(
-        (acc, row) => acc + row.price * row.quantity,
-        0,
-    );
-    const updatePremium = (index: number, value: number) => {
-        setUltraPremiumRows((prevRows) => {
-            const newRows = [...prevRows];
-            newRows[index].quantity = value;
-            return newRows;
-        });
-    };
+  const grandTotal: number =
+    linkPriceTotal + contentPriceTotal + premiumTotal + technicalPriceTotal;
 
-    const grandTotal = rows.reduce(
-        (acc, row) => acc + row.price * row.quantity,
-        premiumTotal,
-    );
+  const estimatedLinks: number =
+    rows.reduce((acc, row) => acc + row.estimate * row.quantity, 0) +
+    ultraPremiumRows.reduce((acc, row) => acc + row.estimate * row.quantity, 0);
 
-    const estimatedLinks = rows.reduce(
-        (acc, row) => acc + row.estimate * row.quantity,
-        0,
-    );
-
-    return (
-        <div className="quote-generator-wrapper">
-            <div className="quote-generator-header">
-                <h1>Link Building Quote Generator</h1>
-            </div>
-            <div className="quote-generator-table-wrapper">
-                <div className="service-row-head">
-                    <p>Link Building Services</p>
-                    <p>Price</p>
-                    <p>Qty</p>
-                    <p>Total</p>
-                    <p>Link Estimate</p>
-                </div>
-                {rows.map((row, index) => {
-                    return (
-                        <div className="service-row">
-                            <p>{row.name}</p>
-                            <p>{row.price}</p>
-                            <input
-                                type="number"
-                                value={row.quantity}
-                                onChange={(e) => updateQuantity(index, Number(e.target.value))}
-                            />
-                            <p>{row.price * row.quantity}</p>
-                            <p>{row.quantity * row.estimate}</p>
-                        </div>
-                    );
-                })}
-                <div className="service-row">
-                    <p id="ultra-premium">
-                        Ultra Premium Links{' '}
-                        <div
-                            id="ultra-premium-checkbox"
-                            className={showUltraPremium ? 'show-ultra' : 'hide-ultra'}
-                            onClick={() => {
-                                if (showUltraPremium) {
-                                    setShowUltraPremium(false);
-                                } else setShowUltraPremium(true);
-                            }}
-                        ></div>
-                    </p>
-                </div>
-                {showUltraPremium && (
-                    <div className="ultra-premium-wrapper">
-                        <div className="ultra-premium-row">
-                            <p></p>
-                            <p></p>
-                            <p>DR</p>
-                            <p>DA</p>
-                            <p>Traffic</p>
-                            <p>Price</p>
-                        </div>
-                        {ultraPremiumRows.map((row, index) => {
-                            return (
-                                <div className="ultra-premium-row">
-                                    <input
-                                        type="number"
-                                        value={row.quantity}
-                                        onChange={(e) =>
-                                            updatePremium(index, Number(e.target.value))
-                                        }
-                                    />
-                                    <p>{row.name}</p>
-                                    <p>{row.dr}</p>
-                                    <p>{row.da}</p>
-                                    <p>{row.traffic}</p>
-                                    <p>{row.price}</p>
-                                </div>
-                            );
-                        })}
-                    </div>
-                )}
-                <div className="service-row-foot">
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p>{grandTotal}</p>
-                    <p>{estimatedLinks}</p>
-                </div>
-            </div>
+  return (
+    <div className="quote-generator-wrapper">
+      <div className="quote-generator-header">
+        <h1>Link Building Quote Generator</h1>
+      </div>
+      <div className="quote-generator-table-wrapper">
+        <div className="service-row-head">
+          <h3>Link Building Services</h3>
+          <p>Price</p>
+          <p>Qty</p>
+          <p>Total</p>
+          <p>Link Estimate</p>
         </div>
-    );
+        {rows.map((row, index) => {
+          return (
+            <div className="service-row">
+              <p>{row.name}</p>
+              <p>{formatDollar.format(row.price)}</p>
+              <input
+                type="number"
+                value={row.quantity}
+                onChange={(e) =>
+                  updateQuantity('links', index, Number(e.target.value))
+                }
+              />
+              <p>{formatDollar.format(row.price * row.quantity)}</p>
+              <p>{row.quantity * row.estimate}</p>
+            </div>
+          );
+        })}
+        <div className="service-row">
+          <h3 id="ultra-premium">
+            Ultra Premium Links{' '}
+            <div
+              id="ultra-premium-checkbox"
+              className={showUltraPremium ? 'show-ultra' : 'hide-ultra'}
+              onClick={() => {
+                if (showUltraPremium) {
+                  setShowUltraPremium(false);
+                } else setShowUltraPremium(true);
+              }}
+            ></div>
+          </h3>
+        </div>
+        {showUltraPremium && (
+          <div className="ultra-premium-wrapper">
+            <div className="ultra-premium-row">
+              <p></p>
+              <p></p>
+              <p>DR</p>
+              <p>DA</p>
+              <p>Traffic</p>
+              <p>Price</p>
+              <p>Link Estimate</p>
+            </div>
+            {ultraPremiumRows.map((row, index) => {
+              return (
+                <div className="ultra-premium-row">
+                  <input
+                    type="number"
+                    value={row.quantity}
+                    onChange={(e) =>
+                      updatePremium(index, Number(e.target.value))
+                    }
+                  />
+                  <p>{row.name}</p>
+                  <p>{row.dr}</p>
+                  <p>{row.da}</p>
+                  <p>{row.traffic}</p>
+                  <p>{formatDollar.format(row.price)}</p>
+                  <p>{row.quantity * row.estimate}</p>
+                </div>
+              );
+            })}
+          </div>
+        )}
+        <h3>Content Services</h3>
+        {contentServiceRows.map((row, index) => {
+          return (
+            <div className="service-row">
+              <p>{row.name}</p>
+              <p>{formatDollar.format(row.price)}</p>
+              <input
+                type="number"
+                value={row.quantity}
+                onChange={(e) =>
+                  updateQuantity('content', index, Number(e.target.value))
+                }
+              />
+              <p>{formatDollar.format(row.price * row.quantity)}</p>
+            </div>
+          );
+        })}
+        <h3>Technical Services</h3>
+        {technicalServiceRows.map((row, index) => {
+          return (
+            <div className="service-row">
+              <p>{row.name}</p>
+              <p>{formatDollar.format(row.price)}</p>
+              <input
+                type="number"
+                value={row.quantity}
+                onChange={(e) =>
+                  updateQuantity('technical', index, Number(e.target.value))
+                }
+              />
+              <p>{formatDollar.format(row.price * row.quantity)}</p>
+            </div>
+          );
+        })}
+        <div className="service-row-foot">
+          <p></p>
+          <p></p>
+          <p></p>
+          <p>Total</p>
+          <p>Total Estimated Links</p>
+        </div>
+        <div className="service-row-foot">
+          <p></p>
+          <p></p>
+          <p></p>
+          <p>{formatDollar.format(grandTotal)}</p>
+          <p>{estimatedLinks}</p>
+        </div>
+        <div className="service-row-foot">
+          <p></p>
+          <p></p>
+          <p>Monthly Term</p>
+          <p>Monthly Cost</p>
+          <p>Cost Per Link</p>
+        </div>
+        <div className="service-row-foot">
+          <p></p>
+          <p></p>
+          <input
+            type="number"
+            name="monthly-term"
+            value={monthlyTerm}
+            onChange={(e) => setMonthlyTerm(Number(e.target.value))}
+          />
+          <p>{formatDollar.format(grandTotal / monthlyTerm)}</p>
+          <p>{formatDollar.format(grandTotal / estimatedLinks)}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/modules/Quote-Generator/index.tsx
+++ components/modules/Quote-Generator/index.tsx
@@ -20,6 +20,14 @@ interface HubspotFieldValues {
     traffic: string;
     price: number;
   }[];
+  contentServices: {
+    name: string;
+    price: number;
+  }[];
+  technicalServices: {
+    name: string;
+    price: number;
+  }[];
 }
 
 interface ModuleProps {
@@ -39,6 +47,7 @@ export function Component(props: ModuleProps): JSX.Element {
       da: 94,
       traffic: '7.2M',
       price: 36000,
+      estimate: 1,
     },
     {
       name: 'rollingstone.com',
@@ -46,8 +55,26 @@ export function Component(props: ModuleProps): JSX.Element {
       da: 92,
       traffic: '3.6M',
       price: 30000,
+      estimate: 1,
     },
-    { name: 'coindesk', dr: 90, da: 91, traffic: '2.3M', price: 26000 },
+    {
+      name: 'coindesk',
+      dr: 90,
+      da: 91,
+      traffic: '2.3M',
+      price: 26000,
+      estimate: 1,
+    },
+  ];
+  const defaultContentServices = [
+    { name: 'Linkable Content', price: 700 },
+    { name: 'Keyword Content', price: 700 },
+    { name: 'Content Road Map', price: 1500 },
+  ];
+  const defaultTechnicalServices = [
+    { name: 'Advanced Content', price: 1200 },
+    { name: 'Website Audit', price: 2000 },
+    { name: 'Schema Optimization', price: 300 },
   ];
   const initialServices =
     props.fieldValues?.services && props.fieldValues.services.length > 0
@@ -58,11 +85,23 @@ export function Component(props: ModuleProps): JSX.Element {
     props.fieldValues.ultraPremiums.length > 0
       ? props.fieldValues.ultraPremiums
       : defaultUltraPremiums;
+  const initialContentServices =
+    props.fieldValues?.contentServices &&
+    props.fieldValues.contentServices.length > 0
+      ? props.fieldValues.contentServices
+      : defaultContentServices;
+  const initialTechnicalServices =
+    props.fieldValues?.technicalServices &&
+    props.fieldValues.technicalServices.length > 0
+      ? props.fieldValues.technicalServices
+      : defaultTechnicalServices;
   return (
     <Island
       module={QuoteGenerator}
       initialServices={initialServices}
       initialUltraPremiums={initialUltraPremiums}
+      initialContentServices={initialContentServices}
+      initialTechnicalServices={initialTechnicalServices}
     />
   );
 }
@@ -71,7 +110,7 @@ export const fields = (
   <ModuleFields>
     <RepeatedFieldGroup
       name="services"
-      label="Service Line Items"
+      label="Link Building Service Line Items"
       default={[
         { name: 'Editorial Links', price: 450, estimate: 1 },
         { name: 'Expert Links', price: 5000, estimate: 6 },
@@ -80,6 +119,11 @@ export const fields = (
     >
       <TextField name="name" label="Item Name" default="Service Item" />
       <NumberField name="price" label="Unit Price ($)" default={0} />
+      <NumberField
+        name="estimate"
+        label="Link Estimate Multiplier"
+        default={1}
+      />
     </RepeatedFieldGroup>
     <RepeatedFieldGroup
       name="ultraPremiums"
@@ -91,6 +135,7 @@ export const fields = (
           da: 94,
           traffic: '7.2M',
           price: 36000,
+          estimate: 1,
         },
         {
           name: 'rollingstone.com',
@@ -98,8 +143,48 @@ export const fields = (
           da: 92,
           traffic: '3.6M',
           price: 30000,
+          estimate: 1,
         },
-        { name: 'coindesk', dr: 90, da: 91, traffic: '2.3M', price: 26000 },
+        {
+          name: 'coindesk',
+          dr: 90,
+          da: 91,
+          traffic: '2.3M',
+          price: 26000,
+          estimate: 1,
+        },
+      ]}
+    >
+      <TextField name="name" label="Item Name" default="Ultra Premium Item" />
+      <NumberField name="dr" label="DR" default={0} />
+      <NumberField name="da" label="DA" default={0} />
+      <TextField name="traffic" label="Traffic" default="7.2M" />
+      <NumberField name="price" label="Unit Price ($)" default={0} />
+      <NumberField
+        name="estimate"
+        label="Link Estimate Multiplier"
+        default={1}
+      />
+    </RepeatedFieldGroup>
+    <RepeatedFieldGroup
+      name="contentServices"
+      label="Content Service Line Items"
+      default={[
+        { name: 'Linkable Content', price: 700 },
+        { name: 'Keyword Content', price: 700 },
+        { name: 'Content Road Map', price: 1500 },
+      ]}
+    >
+      <TextField name="name" label="Item Name" default="Service Item" />
+      <NumberField name="price" label="Unit Price ($)" default={0} />
+    </RepeatedFieldGroup>
+    <RepeatedFieldGroup
+      name="technicalServices"
+      label="Technical Service Line Items"
+      default={[
+        { name: 'Advanced Content', price: 1200 },
+        { name: 'Website Audit', price: 2000 },
+        { name: 'Schema Optimization', price: 300 },
       ]}
     >
       <TextField name="name" label="Item Name" default="Service Item" />
